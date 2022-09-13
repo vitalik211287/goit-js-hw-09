87fccf4c41a23a1433af7b1780b9cdd2a36513aa
vitalik poliakov <vitalik211287@gmail.com>
2022-09-14 01:11:36 +0300

    Timer *(i+1)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,29 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const refs = {
+    delayEl: document.querySelector('input[name=delay]'),
+    stepEl: document.querySelector('input[name=step]'),
+    amountEl: document.querySelector('input[name=amount]'),
+  submitEl: document.querySelector('button[type=submit]'),
+};
+
+
+let inputDelayValue = refs.delayEl.value;
+let inputstepValue = refs.stepEl.value;
+// inputDelayValue = 1000;
+// inputstepValue = 1000;
+const totalDelay = inputDelayValue + inputstepValue;
+// console.log(totalDelay);
+
+refs.submitEl.addEventListener('click', () => {
+   let num = 0
+    function countNum() {
+        num += 1
+        console.log(num);
+    }
+    for (let i = 0; i < refs.amountEl.value; i++) {
+      setTimeout(countNum, totalDelay * (i + 1));
+    }
+});
