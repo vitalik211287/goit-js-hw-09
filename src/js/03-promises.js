6dbe5e58eccbbf0bda4878ac4d870bf2f14281aa
vitalik poliakov <vitalik211287@gmail.com>
2022-09-15 18:57:44 +0300

    Succes  createPromise function

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,12 +15,8 @@ refs.formEl.addEventListener('submit', e => {
   for (let position = 1; position <= amountValue; position += 1) {
     setTimeout(() => {
       createPromise(position, delay)
-        .then(cuccess => {
-          console.log(cuccess);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+        .then(succses=>{succses})
+        .catch(error=>{error});
     }, delay);
 
     delay += inputStepValue;
@@ -31,26 +27,17 @@ const createPromise = (position, delay) => {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     if (shouldResolve) {
-      resolve(onSuccess());
+      resolve(onSuccess(position, delay));
     } else {
-      reject(onErrore());
+      reject(onErrore(position, delay));
     }
   });
 };
-// createPromise(2, 1500)
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error));
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+
 
 function onSuccess(position, delay) {
-  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  return console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
 }
 function onErrore(position, delay) {
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+ return console.log(`❌ Rejected promise ${position} in ${delay}ms`);
 }
